f24b57990bf496867cea85e3eae8b5e3da647863
DylanCheetah <dylan_the_cheetah@outlook.com>
2024-03-07 16:58:49 -0500

    Fix code style in admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -9,14 +9,17 @@ class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 5
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ["name"]
+
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     list_display = ["name"]
     inlines = [CarModelInline]
+
 
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
