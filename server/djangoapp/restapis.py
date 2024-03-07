ce94a280197ccab8a5bbf4350ed484ef2a890362
DylanCheetah <dylan_the_cheetah@outlook.com>
2024-03-07 12:56:40 -0500

    Fix code style in restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -28,8 +28,9 @@ def get_request(endpoint, **kwargs):
         response = requests.get(request_url)
         return response.json()
 
-    except Exception as e:
+    except Exception as err:
         # If any error occurs
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
 
@@ -54,5 +55,6 @@ def post_review(data_dict):
         print(response.json())
         return response.json()
 
-    except Exception as e:
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
