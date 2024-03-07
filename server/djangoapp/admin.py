f03e316ff783033fcd81ff46adb01c74b22b421d
DylanCheetah <dylan_the_cheetah@outlook.com>
2024-03-07 16:56:50 -0500

    Add inline car models to car make admin.

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,11 +5,19 @@ from .models import CarMake, CarModel
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ["name"]
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = [CarModelInline]
 
 # Register models here
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
