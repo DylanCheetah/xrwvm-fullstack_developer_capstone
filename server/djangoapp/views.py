517a8e001442ff64c1764caac3d64fd871ffee3f
DylanCheetah <dylan_the_cheetah@outlook.com>
2024-03-07 13:10:48 -0500

    Fix code style in views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -87,7 +87,7 @@ def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
 
-    if(count == 0):
+    if (count == 0):
         initiate()
 
     car_models = CarModel.objects.select_related('car_make')
@@ -95,7 +95,7 @@ def get_cars(request):
 
     for car_model in car_models:
         cars.append({
-            "CarModel": car_model.name, 
+            "CarModel": car_model.name,
             "CarMake": car_model.car_make.name
         })
 
@@ -105,7 +105,7 @@ def get_cars(request):
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if (state == "All"):
         endpoint = "/fetchDealers"
 
     else:
@@ -117,7 +117,7 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
-    if(dealer_id):
+    if (dealer_id):
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
 
@@ -134,7 +134,7 @@ def get_dealer_reviews(request, dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-    if(dealer_id):
+    if (dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
@@ -145,15 +145,18 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if (not request.user.is_anonymous):
         data = json.loads(request.body)
 
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
 
-        except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+        except Exception:
+            return JsonResponse({
+                "status": 401,
+                "message": "Error in posting review"
+            })
 
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
